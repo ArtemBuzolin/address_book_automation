1c4b8b2254c5b5db3d4808e478fb977bd1555b9f
ArtemBuzolin <prem1um@mail.ru>
2019-10-04 20:13:57 -0400

    randomization by index
    group will be selected by random index

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,15 +1,22 @@
 from model.group_params import GroupParams
+from random import randrange
+
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(GroupParams(name = 'nnn', header = 'www', footer= 'sss'))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = GroupParams(name='New group')
-    group.id = old_groups[0].id
-    app.group.modify_firstgroup(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=GroupParams.id_or_max) == sorted(new_groups, key=GroupParams.id_or_max)
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(GroupParams(name = 'nnn', header = 'www', footer= 'sss'))
     old_groups = app.group.get_group_list()
     app.group.modify_firstgroup(GroupParams(header='New Header'))
     new_groups = app.group.get_group_list()
